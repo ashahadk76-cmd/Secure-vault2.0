9c2b0d584522a1477b6610ed62da7a15f2f8f53f
Ashahad khan <ashahadk76@gmail.com>
2025-11-24 18:52:57 +0530

    eight-commit

--- src/db/connectDb.js
+++ src/db/connectDb.js
@@ -1,32 +1,11 @@
-// import mongoose from "mongoose";
-
-// const connectDB = async ()=>{
-//     try {
-//         const conn = await mongoose.connect(process.env.MONGO_URI);
-//     } catch (error) {
-//         // console.log(error.message)
-//         process.exit(1)
-//     }
-// }
-// export default connectDB;
-
-
 import mongoose from "mongoose";
 
-const connectDB = async () => {
-    if (mongoose.connection.readyState === 1) {
-        return;
-    }
-
+const connectDB = async ()=>{
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            dbName: "securevault",
-        });
-        console.log("MongoDB connected");
+        const conn = await mongoose.connect(process.env.MONGO_URI);
     } catch (error) {
-        console.error("DB CONNECTION ERROR:", error.message);
-        // ❌ NEVER USE process.exit() in serverless
+        console.log(error.message)
+        process.exit(1)
     }
-};
-
+}
 export default connectDB;
